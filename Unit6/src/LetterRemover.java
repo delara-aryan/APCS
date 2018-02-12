f7305b924a69905fcda213d1d82f2cce8f84cedc
delara-aryan <delara.aryan@gmail.com>
2018-02-12 11:13:47 -0800

    started Unit 6

---
+++ Unit6/src/LetterRemover.java
@@ -0,0 +1,44 @@
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+import static java.lang.System.*;
+
+public class LetterRemover
+{
+   private String sentence;
+   private char lookFor;
+
+	public LetterRemover()
+	{
+		setRemover("Hello World!",'d');
+	}
+
+	public LetterRemover(String s, char rem) {
+		setRemover(s, rem);
+	}
+	
+	public void setRemover(String s, char rem)
+	{
+		sentence = s;
+		lookFor = rem;
+	}
+
+	public String removeLetters()
+	{
+		String cleaned=sentence;
+		int loc = cleaned.indexOf(lookFor);
+		while (cleaned.contains("" + lookFor)) {
+			cleaned = cleaned.substring(0, loc) + cleaned.substring(loc + 1, cleaned.length());
+			loc = cleaned.indexOf(lookFor);
+		}
+		return cleaned;
+	}
+
+	public String toString()
+	{
+		return sentence + " - letter to remove " + lookFor + "\n" + removeLetters();
+	}
+}
