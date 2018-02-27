74d2cf25a29fa61dc4aff033dc884f45e4634db0
delara-aryan <delara.aryan@gmail.com>
2018-02-27 10:33:44 -0800

    eh

--- Unit8/src/Grades.java
+++ Unit8/src/Grades.java
@@ -1,3 +1,4 @@
+
 //© A+ Computer Science  -  www.apluscompsci.com
 //Name -
 //Date -
@@ -8,60 +9,47 @@ import java.lang.System;
 import java.lang.Math;
 import java.util.Scanner;
 
-public class Grades
-{
-	//instance variables
+public class Grades {
+	// instance variables
 	Double[] b;
 	int size;
 
-	//constructor
-	public Grades(int amount, Double[]a)
-	{
+	// constructor
+	public Grades(int amount, Double[] a) {
 		setGrades(amount, a);
 	}
 
-
-	//set method
-	public void setGrades(int amount, Double[] a )
-	{
+	// set method
+	public void setGrades(int amount, Double[] a) {
 		size = amount;
 		b = a;
 	}
 
-
-	private double getSum()
-	{
-		double sum=0.0;
+	private double getSum() {
+		double sum = 0.0;
 		for (int i = 0; i < b.length; i++) {
-			sum+=b[i];
+			sum += b[i];
 		}
-
 
 		return sum;
 	}
 
-	public double getAverage()
-	{
-		double average=0.0;
-		average = getSum()/size;
-
+	public double getAverage() {
+		double average = 0.0;
+		average = getSum() / size;
 
 		return average;
 	}
 
-
-	//toString method
-	public String toString()
-	{
+	// toString method
+	public String toString() {
 		String s = "";
 		for (int i = 0; i < b.length; i++) {
-			s+= "grade "+i+" ::     "+b[i] + "\n";
+			s += "grade " + i + " ::     " + b[i] + "\n";
 		}
-		s+="\n average = " + getAverage();
+		s += "\n average = " + getAverage();
 		return s;
-		
+
 	}
-
-
 
 }
