4a1c86bad011e3a90b177fffac9e543f258e8f5d
delara-aryan <delara.aryan@gmail.com>
2018-03-20 11:43:34 -0700

    started elevens

--- Unit12/src/lab18d/Word.java
+++ Unit12/src/lab18d/Word.java
@@ -22,13 +22,9 @@ public class Word implements Comparable<Word>
 		} else {
 			for (int i = 0; i < word.length(); i++) {
 				if(word.charAt(i) < w.charAt(i)) {
-<<<<<<< HEAD
-					return 
-=======
 					return -1;
 				} else if (word.charAt(i) > w.charAt(i)) {
 					return 1;
->>>>>>> 75522839af370e60c5f3fc0427f8c4d3d7f4686d
 				}
 			}
 		}
