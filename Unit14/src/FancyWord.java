168707e2503415ed5ed9d44791271043380f73fe
Delara <delara.aryan@gmail.com>
2018-03-27 09:26:59 -0700

    Uint 14 - FancyWord and TicTacToe

--- Unit14/src/FancyWord.java
+++ Unit14/src/FancyWord.java
@@ -1,3 +1,4 @@
+
 //© A+ Computer Science  -  www.apluscompsci.com
 //Name -
 //Date -
@@ -7,31 +8,36 @@
 import java.util.Scanner;
 import static java.lang.System.*;
 
-public class FancyWord
-{
+public class FancyWord {
 	private char[][] mat;
 
-	public FancyWord()
-	{
-
+	public FancyWord() {
+		mat = new char[0][0];
 	}
 
-   public FancyWord(String s)
-	{
-
-
-
-
+	public FancyWord(String s) {
+		int size = s.length();
+		mat = new char[size][size];
+		for (int i = 0; i < mat.length; i++) {
+			if (i == 0 || i == size - 1) {
+				for (int j = 0; j < mat[i].length; j++) {
+					mat[i][j] = s.charAt(j);
+				}
+			} else {
+				mat[i][i] = s.charAt(i);
+				mat[i][mat[i].length - 1 - i] = s.charAt(mat[i].length - 1 - i);
+			}
+		}
 	}
 
-	public String toString()
-	{
-		String output="";
-
-
-
-
-
-		return output+"\n\n";
+	public String toString() {
+		String output = "";
+		for (int i = 0; i < mat.length; i++) {
+			for (int j = 0; j < mat[i].length; j++) {
+				output += mat[i][j];
+			}
+			output += "\n";
+		}
+		return output + "\n";
 	}
 }
--- Unit14/src/Lab24d.java
+++ Unit14/src/Lab24d.java
@@ -1,29 +1,18 @@
-//© A+ Computer Science  -  www.apluscompsci.com
-//Name -
-//Date -
-//Class - 
-//Lab  -
-
 import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
 import static java.lang.System.*;
 
-public class Lab24d
-{
-	public static void main( String args[] ) throws IOException
-	{
-		Scanner file = new Scanner (new File("Lab24d.dat"));
+public class Lab24d {
+	public static void main(String args[]) throws IOException {
+		Scanner file = new Scanner(new File("Lab24d.dat"));
 
-
-
-
-
-
-
-
+		int size = file.nextInt();
+		file.nextLine();
+		for (int i = 0; i < size; i++) {
+			String word = file.next();
+			TicTacToe w = new TicTacToe(word);
+			System.out.println(w);
+		}
 	}
 }
-
-
-
