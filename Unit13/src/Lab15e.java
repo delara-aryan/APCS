24d36fd70a734a677bfb61d367372429e37dced2
Delara <delara.aryan@gmail.com>
2018-03-30 08:12:48 -0700

    Unit 13

---
+++ Unit13/src/Lab15e.java
@@ -0,0 +1,63 @@
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+import java.util.Arrays;
+import static java.lang.System.*;
+
+public class Lab15e
+{
+	public static void main(String args[])
+	{
+		//test case 1
+		int[] one = NumberSort.getSortedDigitArray(567891);
+		for(int item : one)
+		{
+			System.out.print(item + " ");
+		}
+		System.out.println();
+		
+		//test case 2	
+		
+		int[] one2 = NumberSort.getSortedDigitArray(129814702);
+		for(int item : one2)
+		{
+			System.out.print(item + " ");
+		}
+		System.out.println();
+	
+	
+	
+	
+	
+	
+		
+		
+		
+			
+
+		//test case 3
+		
+		int[] one1 = NumberSort.getSortedDigitArray(43147829);
+		for(int item : one1)
+		{
+			System.out.print(item + " ");
+		}
+		System.out.println();
+		
+	
+	
+	
+	
+	
+	
+		
+		
+		
+		
+		
+
+	}
+}
---
+++ Unit13/src/Lab22f.java
@@ -0,0 +1,22 @@
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class - 
+//Lab  - 
+
+public class Lab22f
+{
+	public static void main(String args[])
+	{
+		QuickSort s = new QuickSort();
+		s.quickSort(new Comparable[]{9,5,3,2});
+		s.zero();
+		System.out.println("\n");
+		s.quickSort(new Comparable[]{19,52,3,2,7,21});
+		s.zero();
+		System.out.println("\n");
+		s.quickSort(new Comparable[]{68,66,11,2,42,31});
+		s.zero();
+		System.out.println("\n");
+	}
+}
